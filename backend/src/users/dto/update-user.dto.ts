fffc68ce7254889126388be24b4bde79d35ae609
Emanuel <emanuelcabral8@outlook.com>
2025-10-15 02:53:44 +0000

    feat: user service logic done, added role and name in schema.prisma

--- backend/src/users/dto/update-user.dto.ts
+++ backend/src/users/dto/update-user.dto.ts
@@ -1,4 +1,28 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateUserDto } from './create-user.dto';
+import {
+    IsOptional, IsBoolean, IsString,
+    IsNotEmpty,
+    MinLength, IsEmail, MaxLength
+} from 'class-validator';
 
-export class UpdateUserDto extends PartialType(CreateUserDto) {}
+export class UpdateUserDto extends PartialType(CreateUserDto) {
+    @IsOptional()
+    @IsBoolean({ message: 'isActive must be a boolean' })
+    isActive?: boolean;
+
+    @IsOptional()
+    @MinLength(6, { message: 'Password must be at least 6 characters long' })
+    @IsString({ message: 'Password must be of type string' })
+    @MaxLength(100, {
+        message: 'Password must not exceed 100 characters.',
+      })
+    password?: string;
+
+    @IsOptional()
+    @IsEmail()
+    
+    email?: string;
+
+
+}
--- backend/src/users/users.service.ts
+++ backend/src/users/users.service.ts
@@ -1,41 +1,89 @@
 import { Injectable } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
-import { PrismaClient } from '@prisma/client/extension';
+import { PrismaService } from 'src/prisma/prisma.service';
+import * as bcrypt from 'bcrypt';
+import { SafeUser } from 'src/types/user.types';
+
 
 @Injectable()
 export class UsersService {
-  private prisma = new PrismaClient();
+  constructor(private readonly prisma: PrismaService) { }
 
-  async create(createUserDto: CreateUserDto) {
-    return 'This action adds a new user';
+  // Use of select to avoid exposing the hashed password to the frontend
+  private readonly safeUserSelect = {
+    id: true,
+    email: true,
+    birthdate: true,
+    isActive: true,
+    createdAt: true,
+    updatedAt: true,
+    name:true,
+    role: true
+  };
+  
+  private readonly saltRounds = 10
+
+  async create(createUserDto: CreateUserDto): Promise<SafeUser> {
+    const hash = await this.hashPassword(createUserDto.password)
+    const newUser = await this.prisma.user.create({
+      data: {
+        name: createUserDto.name,
+        email: createUserDto.email,
+        birthdate: createUserDto.birthdate,
+        password: hash,
+        isActive: true
+      },
+      select: this.safeUserSelect
+    })
+
+    return newUser;
   }
 
   async findAll() {
-    return this.prisma.users.findMany();
+    return this.prisma.user.findMany({select: this.safeUserSelect});
   }
 
   async findOneById(id: string) {
-    return this.prisma.users.findUnique({where:{id}});
+    return this.prisma.user.findUnique({ where: { id }, select: this.safeUserSelect });
   }
 
-  async findOneByEmail(email:string) {
-    return this.prisma.users.findUnique({where:{email}});
+  async findOneByEmail(email: string) {
+    return this.prisma.user.findUnique({ where: { email }, select: this.safeUserSelect });
   }
 
-  async updateById(id: string, updateUserDto: UpdateUserDto) {
+  async updateById(id: string, updateUserDto: UpdateUserDto) : Promise <SafeUser> {
     try {
-      return await this.prisma.users.update({where:{id}, data: updateUserDto})
-    } catch(err:any) {
+      if (updateUserDto.password) {
+        const saltRounds: number = 10
+        updateUserDto.password = await this.hashPassword(updateUserDto.password)
+      }
+      return await this.prisma.user.update({
+        where: { id }, data: updateUserDto, select: this.safeUserSelect
+      })
+    } catch (err: any) {
       throw new Error(`Could not update user: ${err.message}`)
     }
   }
 
-  async removeById(id: string) {
-      try {
-      return await this.prisma.users.delete({where:{id}});
-    } catch(err:any) {
+  async activateUser(id: string): Promise<SafeUser> {
+    return await this.updateById(id, { isActive: true });
+  }
+
+  async deactivateUser(id: string): Promise<SafeUser> {
+    return await this.updateById(id, { isActive: false });
+  }
+
+
+  async removeById(id: string): Promise<SafeUser> {
+    try {
+      return await this.prisma.user.delete({ where: { id }, select: this.safeUserSelect});
+    } catch (err: any) {
       throw new Error(`Could not delete user: ${err.message}`)
     }
   }
-  }
+  
+  private async hashPassword(password: string): Promise<string> {
+  return bcrypt.hash(password, this.saltRounds);
+}
+}
