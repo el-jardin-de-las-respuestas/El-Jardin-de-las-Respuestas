2049d8fb01e8a09d51a3fa759b424f80a444b56f
fioret20 <fiorellaterranova0@gmail.com>
2025-10-22 23:43:04 -0300

    feat: add Library module (schema, service, controller, and module integration)

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -5,9 +5,10 @@ import { AuthModule } from './auth/auth.module';
 import { Prisma } from 'generated/prisma';
 import { PrismaModule } from './prisma/prisma.module';
 import { PostForoModule } from './post-foro/post-foro.module';
+import { LibraryModule } from './library/library.module';
 
 @Module({
-  imports: [AuthModule,PrismaModule],
+  imports: [AuthModule,PrismaModule, PostForoModule, LibraryModule],
   controllers: [AppController],
   providers: [AppService],
 })
