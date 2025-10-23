8f8baa147fcbd54be2e65b7445e6717812969458
Emanuel Cabral <emanuelcabral8@outlook.com>
2025-10-23 18:22:21 +0000

    feat: add guard for roles

--- backend/src/guards/auth.guard.ts
+++ backend/src/guards/auth.guard.ts
@@ -8,7 +8,7 @@ import {
 import { JwtService } from '@nestjs/jwt';
 import { Request } from 'express';
 import { Reflector } from '@nestjs/core';
-import { IS_PUBLIC_KEY} from '../decorators/public-decorator';
+import { IS_PUBLIC_KEY} from '../decorators/public.decorator';
 
 
 @Injectable()
