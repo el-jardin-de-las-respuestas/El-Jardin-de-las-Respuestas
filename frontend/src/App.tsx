02bf0537647b0324de60ac37dcf7e1b36a880424
agos <agostinadeyurka@yahoo.com>
2025-10-28 16:46:12 -0300

    fix biblioteca ESI redireccion

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -20,7 +20,6 @@ import { ProfessionalRegistrationPage } from './components/pages/ProfessionalReg
 import { ProfessionalLoginPage } from './components/pages/ProfessionalLoginPage';
 
 
-import { BlogPage } from './components/pages/BlogPage';
 import { ComunicationPage } from './components/ComunicationPage';
 import { FAQPage } from './components/pages/FAQPage';
 import { ProfilePage } from './components/ProfilePage';
@@ -116,7 +115,7 @@ function AppContent() {
         return <CommunityPage />;
 
       case "blog":
-        return <BlogPage />;
+         return <LibraryEsi onNavigate={handleNavigate} />;
 
       case 'faq':
         return <FAQPage />;
