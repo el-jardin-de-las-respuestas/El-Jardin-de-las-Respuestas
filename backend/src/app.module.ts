068abb1022c4c5b15ec393b4cdf4df657e6402c6
Emanuel <emanuelcabral8@outlook.com>
2025-11-04 21:21:42 +0000

    as

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -2,7 +2,6 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
-import { Prisma } from 'generated/prisma';
 import { PrismaModule } from './prisma/prisma.module';
 import { PostForoModule } from './post-foro/post-foro.module';
 import { LibraryModule } from './library/library.module';
